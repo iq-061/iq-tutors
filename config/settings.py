b8837ae71b3832e597af70b6e45bd5197957acee
Ibrahim Qamar <iqamar2006@gmail.com>
2026-01-12 20:02:49 +0000

    fix leak

--- config/settings.py
+++ config/settings.py
@@ -20,11 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5)c1kv(!is-*zyc@twqbctt4^rphtxx(@@($0cf4k@1anof=r@'
-
+SECRET_KEY = os.environ.get("SECRET_KEY", "dev-only-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".onrender.com"]
 CSRF_TRUSTED_ORIGINS = ["https://*.onrender.com"]
 
@@ -128,17 +126,17 @@ STORAGES = {
     },
 }
 
-# --- Email (Gmail SMTP) ---
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# # --- Email (Gmail SMTP) ---
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False  # keep False when using TLS on 587
+# EMAIL_HOST = "smtp.gmail.com"
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = False  # keep False when using TLS on 587
 
-EMAIL_HOST_USER = "iqtutors.enquire@gmail.com"
-EMAIL_HOST_PASSWORD = "syhcvxvttqeajtov"
+# EMAIL_HOST_USER = "iqtutors.enquire@gmail.com"
+# EMAIL_HOST_PASSWORD = "syhcvxvttqeajtov"
 
-# Your contact form settings
-CONTACT_TO_EMAIL = "iqtutors.enquire@gmail.com"
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+# # Your contact form settings
+# CONTACT_TO_EMAIL = "iqtutors.enquire@gmail.com"
+# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
