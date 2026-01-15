59cd9205e8e11c1a5ee78ba66dadfb84282884db
Ibrahim Qamar <iqamar2006@gmail.com>
2026-01-15 16:47:19 +0000

    s

--- config/settings.py
+++ config/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY", "dev-only-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.environ.get("DEBUG", "False") == "True"
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
+# DEBUG = True
 ALLOWED_HOSTS = [
     "iq-tutors.co.uk",
     "www.iq-tutors.co.uk",
